adb1d0b3941bc1d711bc56918beb21c951405604
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2021-09-28 19:42:42 +0300

    feat: Added responses and auth/token files
    Responses format the output to json format and token has functions to create and extract JWT token

---
+++ api/responses/responses.go
@@ -0,0 +1,24 @@
+package responses
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Error struct {
+	Error string `json:"error"`
+}
+
+//Formats data to JSON format when request is successful
+func SUCCESS(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
+
+//Formats data into custom error struct response if error occours
+func ERROR(w http.ResponseWriter, statusCode int, err error) {
+	if err != nil {
+		SUCCESS(w, statusCode, Error{err.Error()})
+	}
+	SUCCESS(w, http.StatusBadRequest, nil)
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/badoux/checkmail v1.2.1
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/jinzhu/gorm v1.9.16
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
 )
